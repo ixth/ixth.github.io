ee3ad215e4e0368c1977b2886ee8d51c599482c9
Mikhail Menshikov <ainop@ya.ru>
2016-11-17 14:34:16 +0300

    Update scenes.js

--- js/scenes.js
+++ js/scenes.js
@@ -1,9 +1,14 @@
 Crafty.defineScene('Balrog', function() {
     Crafty.background('url(/i/tile.png) repeat');
 
-    Crafty.e('Floor, 2D, DOM, Color')
-        .attr({ x: 0, y: 600, w: 800, h: 4 })
-        .color('rgb(204, 204, 204)');
+    Crafty.e('Floor, 2D, DOM')
+        .attr({ x: 0, y: 600, w: 800, h: 4 });
+
+    Crafty.e('Floor, 2D, DOM')
+        .attr({ x: -1, y: 0, w: 1, h: 600 });
+
+    Crafty.e('Floor, 2D, DOM')
+        .attr({ x: 800, y: 0, w: 1, h: 600 });
 
     Crafty.e('Balrog')
         .attr({ x: 320 });
